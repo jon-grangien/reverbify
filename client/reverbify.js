86c5315c5508eed5cacfb23ee20115c950f6ebcb
codingInSpace <jonathan.grangien@gmail.com>
2015-09-22 12:01:07 +0200

    implement image upload test
    can upload an image to a folder ~/uploads/ on the local storage

--- client/reverbify.js
+++ client/reverbify.js
@@ -8,7 +8,21 @@ Template.hello.helpers({
 
 Template.hello.events({
   'click button': function () {
-    // increment the counter when button is clicked
     Session.set('counter', Session.get('counter') + 1);
+  }
+});
+
+Template.uploadTest.events({
+	// simple test
+	'change .fileUploader': function(event, template) {
+    var files = event.target.files;
+    for (var i = 0, ln = files.length; i < ln; i++) {
+      Images.insert(files[i], function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      	if (err) {
+      		console.log("Error: " + err);
+      	}
+      });
+    }
   }
 });
--- server/reverbify.js
+++ server/reverbify.js
@@ -1,3 +1,11 @@
-Meteor.startup(function () {
+// Meteor.startup(function () {
   // code to run on server at startup
+// });
+
+// Allow users to upload images to image collection
+Images.allow({
+  'insert': function () {
+    // add custom authentication code here
+    return true;
+  }
 });
