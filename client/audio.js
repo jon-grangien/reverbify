8284bf9e436740a82d3ad967c430abcd22dcca08
Benjamin Wiberg <benjaminwiberg@gmail.com>
2015-09-29 11:00:46 +0200

    Implement loading of audio file from server

--- client/audio.js
+++ client/audio.js
@@ -1,24 +1,55 @@
 // Load Web Audio API context
 Reverbify.AudioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-// Initialize loadedAudio to null
-Reverbify.decodedAudioData = null;
-
+/**
+ * Loads the audio file located at "path" using the Web Audio API, and calls onLoad when completed
+ * @param path The path to the audio file
+ * @param onLoad Callback {{ function(didLoad, audioBuffer) }}. If didLoad is true, audioBuffer contains the loaded data.
+ * If didLoad is false, audioBuffer is null.
+ */
 Reverbify.loadAudio = function (path, onLoad) {
   // Load the audio file using a GET request
-  var request = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-  request.open('GET', path, true);
-  request.responseType = 'arraybuffer';
+  xhr.responseType = 'arraybuffer';
+  xhr.open('GET', path, true);
 
-  request.onload = function () {
+  xhr.onload = function () {
     // The arraybuffer contains the audio data from the given file
-    var audioData = request.response;
+    var audioData = xhr.response;
 
-    Reverbify.AudioCtx.decodeAudioData(audioData).then(function (decodedData) {
-      // Here we use the decoded data
-      Reverbify.decodedAudioData = decodedData;
-      onLoad();
-    });
+    Reverbify.AudioCtx.decodeAudioData(
+        audioData,
+        function (decodedData) {
+          // The audio data loaded successfully, callback with success and the data
+          onLoad(true, decodedData);
+        },
+        function (err) {
+          // The audio data failed to load, callback with failure and AudioBuffer = null
+          console.log("Error while decoding audio data: " + err);
+          onLoad(false, null);
+        }
+    );
   };
-}
+
+  xhr.send();
+};
+
+// Test the loadAudio function
+(function () {
+  console.log('Test loadAudio function');
+  Reverbify.loadAudio('/audio/K4_impulse_response.wav', function (didLoad, audioBuffer) {
+    if (didLoad) {
+      console.log('Audio file loaded');
+      console.log(audioBuffer);
+
+      for (var i = 0; i < audioBuffer.numberOfChannels; ++i) {
+        console.log(audioBuffer.getChannelData(i));
+      }
+    }
+
+    else {
+      console.log('Failed to load audio file');
+    }
+  })
+})();
