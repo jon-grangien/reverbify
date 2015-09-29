4d7245284dd90ad33da8eb2953f39edd694068cc
Benjamin Wiberg <benjaminwiberg@gmail.com>
2015-09-29 14:08:41 +0200

    Revert 'click .continue-button' function

--- client/templates/hello.js
+++ client/templates/hello.js
@@ -1,22 +1,24 @@
+var playConvolvedAudio = function () {
+  // Create AudioBufferSourceNode (inherits from AudioNode) and set its buffer to the loaded signal
+  var audioSourceNode = Reverbify.AudioCtx.createBufferSource();
+  audioSourceNode.buffer = Reverbify.Audio.signalBuffer;
+
+  // Create ConvolverNode (inherits from AudioNode) and set its buffer to the loaded kernel
+  var convolverNode = Reverbify.AudioCtx.createConvolver();
+  convolverNode.buffer = Reverbify.Audio.kernelBuffer;
+
+  // Connect the source node's output to the convolver node's input
+  audioSourceNode.connect(convolverNode);
+  // Connect the convolver node's output to the audio player
+  convolverNode.connect(Reverbify.AudioCtx.destination);
+
+  // Play the sound
+  audioSourceNode.start();
+};
+
 Template.hello.events({
   'click .continue-button': function () {
-    // Create AudioBufferSourceNode (inherits from AudioNode) and set its buffer to the loaded signal
-    var audioSourceNode = Reverbify.AudioCtx.createBufferSource();
-    audioSourceNode.buffer = Reverbify.Audio.signalBuffer;
-
-    // Create ConvolverNode (inherits from AudioNode) and set its buffer to the loaded kernel
-    var convolverNode = Reverbify.AudioCtx.createConvolver();
-    convolverNode.buffer = Reverbify.Audio.kernelBuffer;
-
-    // Connect the source node's output to the convolver node's input
-    audioSourceNode.connect(convolverNode);
-    // Connect the convolver node's output to the audio player
-    convolverNode.connect(Reverbify.AudioCtx.destination);
-
-    // Play the sound
-    audioSourceNode.start();
-    //Router.go('/convolve');
-    console.log('swag');
+    Router.go('/convolve');
   },
 
   'click .use-existing-button': function () {
