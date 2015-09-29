cf491426060fc66394dc0d072e73936fc1144560
Benjamin Wiberg <benjaminwiberg@gmail.com>
2015-09-29 15:08:58 +0200

    Add Ionic card that shows chosen environment info

--- client/templates/convolve.js
+++ client/templates/convolve.js
@@ -6,7 +6,23 @@ Template.convolve.events({
   'click .option': function (event, template) {
     var target = event.target;
 
-    var environment = target.innerHTML;
-    alert('Chose ' + environment);
+    var env_title = target.innerHTML;
+    alert('Chose ' + env_title);
+
+    Session.set('env_title', env_title);
+
+    $('.environment-card').removeClass('hidden');
+  }
+});
+
+Session.setDefault('env_title', '>Default title<');
+Session.setDefault('env_image_src', '/images/env_image_default.jpg');
+
+Template.convolve.helpers({
+  env_title: function () {
+    return Session.get('env_title');
+  },
+  env_image_src: function () {
+    return Session.get('env_image_src');
   }
 });
