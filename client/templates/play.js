9a867593d40f8dcdfa4b03fb75611d94de27a646
codingInSpace <jonathan.grangien@gmail.com>
2015-10-02 15:58:38 +0200

    Add android check for different convolution method

--- client/templates/play.js
+++ client/templates/play.js
@@ -1,19 +1,33 @@
 Template.play.events({
   'click .play-button': function () {
-    // Create AudioBufferSourceNode (inherits from AudioNode) and set its buffer to the loaded signal
-    var audioSourceNode = Reverbify.AudioCtx.createBufferSource();
-    audioSourceNode.buffer = Reverbify.Audio.signalBuffer;
+    IonLoading.show();
 
-    // Create ConvolverNode (inherits from AudioNode) and set its buffer to the loaded kernel
-    var convolverNode = Reverbify.AudioCtx.createConvolver();
-    convolverNode.buffer = Reverbify.Audio.kernelBuffer;
+    if( Platform.isAndroid() ) {
+        // var env_title = Reverbify.Audio.kernelBuffer;
+        // var signal = Reverbify.Audio.signalBuffer;
 
-    // Connect the source node's output to the convolver node's input
-    audioSourceNode.connect(convolverNode);
-    // Connect the convolver node's output to the audio player
-    convolverNode.connect(Reverbify.AudioCtx.destination);
+        // var conv = Reverbify.convolution(signal, env_title);
+        IonLoading.hide();
+        alert('Android not supported yet');
+    }
 
-    // Play the sound
-    audioSourceNode.start();
+    else {
+        // Create AudioBufferSourceNode (inherits from AudioNode) and set its buffer to the loaded signal
+        var audioSourceNode = Reverbify.AudioCtx.createBufferSource();
+        audioSourceNode.buffer = Reverbify.Audio.signalBuffer;
+
+        // Create ConvolverNode (inherits from AudioNode) and set its buffer to the loaded kernel
+        var convolverNode = Reverbify.AudioCtx.createConvolver();
+        convolverNode.buffer = Reverbify.Audio.kernelBuffer;
+
+        // Connect the source node's output to the convolver node's input
+        audioSourceNode.connect(convolverNode);
+        // Connect the convolver node's output to the audio player
+        convolverNode.connect(Reverbify.AudioCtx.destination);
+
+        // Play the sound
+        audioSourceNode.start();
+        IonLoading.hide();
+    }
   }
 });
